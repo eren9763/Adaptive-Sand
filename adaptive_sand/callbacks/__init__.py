50d848fdc83e322cbd872682f7756fa0d2075d77
eren9763 <127925927+eren9763@users.noreply.github.com>
2026-07-20 11:19:06 +0300

    Delete adaptive_sand directory

--- adaptive_sand/callbacks/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Callbacks for Adaptive SAND."""
-
-from adaptive_sand.callbacks.pruning import (
-    SANDSmartHybridPruningCallback,
-    SANDSmartPruningCallback,
-)
-
-__all__ = ["SANDSmartPruningCallback", "SANDSmartHybridPruningCallback"]
-
